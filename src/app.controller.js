a43506803cc42643bd0648d64ca8c8ced1d42b49
ahmed <ahmedSamyAlashmony1@gmail.com>
2025-08-27 20:38:19 +0300

    limited requestes from express-rate-limit

--- src/app.controller.js
+++ src/app.controller.js
@@ -1,9 +1,21 @@
 import { connectDB } from "./DB/connecting.js";
-import fs from 'fs';
 import { authRouter, userRouter, messageRouter } from "./modules/index.js";
 import cors from "cors";
 import { globalErrorHandler } from "./utils/error/index.js";
+import { rateLimit } from "express-rate-limit";
 function bootstrap(app, express) {
+  const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    limit: 20,
+    handler: (req, res, next, options) => {
+      throw new Error(options.message, { cause: options.statusCode });
+    },
+    skipSuccessfulRequests: true,
+    identifier: (req, res, next) => {
+      return req.ip;
+    },
+  });
+  app.use("/auth", limiter);
   app.use(express.json());
   app.use("/uploads", express.static("uploads"));
   app.use(
@@ -16,6 +28,6 @@ function bootstrap(app, express) {
   app.use("/message", messageRouter);
   app.use("/user", userRouter);
   //global error handler
-  app.use(globalErrorHandler)
+  app.use(globalErrorHandler);
 }
 export default bootstrap;
